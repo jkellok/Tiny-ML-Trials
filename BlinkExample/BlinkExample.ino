cb289366beedd4f43607a943d9b9f0490d37c326
Juska Kellokumpu <juskakaro.kellokumpu@tuni.fi>
2023-05-04 16:17:28 +0300

    Blink example to test boards

---
+++ BlinkExample/BlinkExample.ino
@@ -0,0 +1,24 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Based on Arduino IDE Examples -> Basic -> Blink
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
+*/
+
+//int ledPin = 33;      // led for ESP32-CAM, replace with LED_BUILTIN
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (LOW for Portenta & Maixduino built-in LED)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);    // turn the LED off by making the voltage HIGH
+  delay(1000);                       // wait for a second
+}
